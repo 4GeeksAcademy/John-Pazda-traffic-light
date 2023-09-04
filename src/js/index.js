263c65092f8ee72c0a30806d7227df18e98e99c2
Jpazda <111332675+Jpazda@users.noreply.github.com>
2023-09-04 05:14:58 +0000

    First Commit

--- src/js/index.js
+++ src/js/index.js
@@ -2,11 +2,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// include your styles into the webpack bundle
 import "../styles/index.css";
+import TrafficLight from "./component/home";
 
-//import your own components
-import Home from "./component/home.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+
+ReactDOM.render(<TrafficLight />, document.querySelector("#app"));
